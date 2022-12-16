0e29efb7edb4e4d1ec4fc2f7d7f5907d9abdf618
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2022-12-16 13:51:44 -0300

    Listed companies array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Globals from "./Globals";
 
 function App() {
+  const companiesData = Globals.companies;
+  const [companies, setCompanies] = useState(companiesData);
+
   return (
     <div className="App">
-      
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Estado</th>
+              <th>nombre</th>
+              <th>Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            {companies.map((company, i) => (
+                <tr key={i}>
+                  <td>{company.id}</td>
+                  <td>{company.status}</td>
+                  <td>{company.name}</td>
+                  <td>{company.date}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
