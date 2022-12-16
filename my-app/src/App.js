52ab856976db28b78d8ec95402e6d073098e5a9f
Ignacio Garcia <ig.ignaciogarcia.ig@gmail.com>
2022-12-16 13:58:31 -0300

    Title component | sort data functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,62 @@
 import "./App.css";
 import React, { useState } from "react";
 import Globals from "./Globals";
+import Title from './Components/Title';
 
 function App() {
   const companiesData = Globals.companies;
   const [companies, setCompanies] = useState(companiesData);
+  const [sort, setSort] = useState(null);
+
+  const sortBy = (element) => {
+    if(!sort){ 
+      // Ascending sort for array element
+      const sortedCompanies = [...companies].sort((a, b) => {
+        if(a[element] < b[element]){
+          return -1
+        }
+      });
+
+      setCompanies(sortedCompanies);
+      setSort(true);
+    } else {
+      // Descending sort for array element
+      const sortedCompanies = [...companies].sort((a, b) => {
+        if(a[element] > b[element]){
+          return -1
+        }
+      });
+
+      setCompanies(sortedCompanies);
+      setSort(false);
+    }
+  }
+
 
   return (
     <div className="App">
+
+      <Title 
+        title="Compañías"
+      />
+
       <div>
         <table>
           <thead>
             <tr>
-              <th>ID</th>
+              <th>
+                ID
+                <button className="btn" onClick={() => sortBy('id')}>↑↓</button>
+              </th>
               <th>Estado</th>
-              <th>nombre</th>
-              <th>Fecha</th>
+              <th>
+                nombre
+                <button className="btn" onClick={() => sortBy('id')}>↑↓</button>
+              </th>
+              <th>
+                Fecha
+                <button className="btn" onClick={() => sortBy('id')}>↑↓</button>
+              </th>
             </tr>
           </thead>
           <tbody>
